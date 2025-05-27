467699fe3971e9285f51eeb2b99a09c65c11c491
ArtCM <arthurperfilprof@hotmail.com>
2025-05-27 08:55:43 -0300

    ajuste para deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  trailingSlash: true,
+  reactStrictMode: true,
 };
 
-export default nextConfig;
-
+module.exports = nextConfig;
